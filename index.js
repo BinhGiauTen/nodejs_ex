813df34e5cd25bcb2f141f04e3dbd4b3e6d337e0
BinhGiauTen <binhpro50@gmail.com>
2023-08-20 23:59:04 +0700

    Modified .gitignore, index.js, node_modules/.package-lock.json, package-lock.json, and package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
+app.use(morgan('combined'))
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
